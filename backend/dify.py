26adfd0f088a622c485c069bd6e86a26639a6ec8
just-ugly <17784017099@163.com>
2026-01-13 14:51:43 +0800

    初始项目

--- backend/dify.py
+++ backend/dify.py
@@ -2,6 +2,13 @@ import requests
 
 
 def dify_request(question: str):
+    """
+    最基本的 Dify 请求封装
+    传入一个问题，返回 Dify 的回答
+
+    :param question: 用户的问题
+    :return: Dify 的回答
+    """
     DIFY_API_KEY = "app-1IwfITLttFa7sNpXP26AN3YN"
     DIFY_URL = "https://api.dify.ai/v1/workflows/run"
 
